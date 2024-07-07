cf527a940ee64032f8968ce3f035ef3593859184
Heri Setyawan <herisetyawan233@gmail.com>
2024-07-07 23:03:16 +0700

    feat: midtrans

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -31,8 +31,31 @@ class BookingController extends Controller
             'biaya_admin' => 'required|string|max:255',
         ]);
 
+        \Midtrans\Config::$serverKey = 'SB-Mid-server-RFTZq0fJlqeTuL9mkkB4gc8M';
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
+        
         $validatedData['user_id'] = Auth::user()->id;
         $booking = Booking::create($validatedData);
+
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => 'PRA-00'.$booking->id,
+                'gross_amount' => intval($request->biaya_layanan) + intval($request->biaya_admin),
+            ),
+            'customer_details' => array(
+                'name' => Auth::user()->name,
+                'email' => Auth::user()->email,
+                
+            ),
+        );
+
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+        $booking['snapToken'] = $snapToken;
         return response()->json($booking, 201);
     }
 
@@ -83,3 +106,39 @@ class BookingController extends Controller
         }
     }
 }
+
+
+/*Install Midtrans PHP Library (https://github.com/Midtrans/midtrans-php)
+composer require midtrans/midtrans-php
+                              
+Alternatively, if you are not using **Composer**, you can download midtrans-php library 
+(https://github.com/Midtrans/midtrans-php/archive/master.zip), and then require 
+the file manually.   
+
+require_once dirname(__FILE__) . '/pathofproject/Midtrans.php'; */
+
+//SAMPLE REQUEST START HERE
+
+// Set your Merchant Server Key
+// \Midtrans\Config::$serverKey = 'YOUR_SERVER_KEY';
+// // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+// \Midtrans\Config::$isProduction = false;
+// // Set sanitization on (default)
+// \Midtrans\Config::$isSanitized = true;
+// // Set 3DS transaction for credit card to true
+// \Midtrans\Config::$is3ds = true;
+
+// $params = array(
+//     'transaction_details' => array(
+//         'order_id' => rand(),
+//         'gross_amount' => 10000,
+//     ),
+//     'customer_details' => array(
+//         'first_name' => 'budi',
+//         'last_name' => 'pratama',
+//         'email' => 'budi.pra@example.com',
+//         'phone' => '08111222333',
+//     ),
+// );
+
+// $snapToken = \Midtrans\Snap::getSnapToken($params);
